ba7b6cb2342726d347ea2f76115b4feaabfc835b
IvanMudrak <ivan.aleksandrovich.mudrak@gmail.com>
2020-03-05 21:50:18 +0200

    edited task 10.7

--- index.php
+++ index.php
@@ -7,10 +7,10 @@ echo'стоимость сыра за кг = '.$cost.'<br/>';
 $cost /= 1000;
 for ($i = 50; $i <= 1000; $i+=50) {
     echo $i.'гр. сыра стоит '.($cost*$i).'<br/>';
-}*/
+}*///5.21 про сыр
 
 
-echo '<h3 style = color:forestgreen>10.2 Рассчитать значение y, определив и использовав необходимую функцию:</h3><br/>';
+/*echo '<h3 style = color:forestgreen>10.2 Рассчитать значение y, определив и использовав необходимую функцию:</h3><br/>';
 function fraction($a,$b){
     return $a/$b;
 }
@@ -49,12 +49,12 @@ $d = third(2,3)
     + third(1,5)
     + fourth(4,7);
 $d = round($d,2);
-echo ' d) y = '.$d.'<br/>';
+echo ' d) y = '.$d.'<br/>';*///10.2 функция у
 
 
 
 
-/*echo '<h3 style = color:forestgreen>10.7.    Даны основания и высоты двух равнобедренных трапеций. Найти сумму их
+echo '<h3 style = color:forestgreen>10.7.    Даны основания и высоты двух равнобедренных трапеций. Найти сумму их
 периметров.  (Определить  функцию  для  расчета  периметра  равнобедренной 
 трапеции по ее основаниям и высоте.)</h3><br/>';
 
@@ -62,7 +62,7 @@ function perimeter($upper_base,$bottom_base,$height)
 {
     $cathet = ($bottom_base - $upper_base) / 2;
     $hypotenuse = sqrt(($height ** 2) + ($cathet ** 2));
-    return $p = round(($hypotenuse * 2 + $upper_base + $bottom_base), 3);
+    return round(($hypotenuse * 2 + $upper_base + $bottom_base), 3);
 }
 
 $upper_base = rand(1, 5);
@@ -72,11 +72,9 @@ echo'нижнее основание первой трапеции = '.$bottom_b
 $height = rand(4, 10);
 echo'высота трапеции = '.$height.'<br/>';
 
-
 echo'Периметр первой трапеции = ';
 $p1 = perimeter($upper_base,$bottom_base,$height);
 echo $p1.'<br/><br/>';
-
 
 $upper_base = rand(1, 5);
 echo'верхнее основание второй трапеции = '.$upper_base.'<br/>';
@@ -85,10 +83,9 @@ echo'нижнее основание второй трапеции = '.$bottom_b
 $height = rand(4, 10);
 echo'высота трапеции = '.$height.'<br/>';
 
-
 echo'Периметр второй трапеции = ';
 $p2 = perimeter($upper_base,$bottom_base,$height);
 echo $p2.'<br/><br/>';
 
 echo'Сумма периметров трапеций = ';
-echo $sum = $p1 + $p2;*/
+echo $sum = $p1 + $p2;//10.7 Сумма периметров трапеций
